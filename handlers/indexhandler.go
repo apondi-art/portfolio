36d75e9bf528e4090d61a660690de1222f699ee2
quochieng <aiyanarich64@gmail.com>
2025-06-09 12:03:01 +0300

    feat:move index.html to main root

--- handlers/indexhandler.go
+++ handlers/indexhandler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-var Temp = template.Must(template.ParseGlob("templates/*.html"))
+var Temp = template.Must(template.ParseGlob("*.html"))
 
 func Homepage(w http.ResponseWriter, r *http.Request) {
 	err := Temp.ExecuteTemplate(w, "index.html", nil)
